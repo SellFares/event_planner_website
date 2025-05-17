3d4288942795831274c11f96a17e4d71adde9182
pkfitzVT <pkeenanfitzgerald@gmail.com>
2025-05-17 14:51:50 -0400

    added custom footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import './EventPlanner.css'; // Import CSS file for styling
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </footer>
+        </>
+    )}
+export default Footer
