97eb63bb0ca2e2f39e2512b8b1a581d098cecf1f
pkfitzVT <pkeenanfitzgerald@gmail.com>
2025-05-17 13:22:16 -0400

    added description section with button details.

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import './EventPlanner.css'; // Import CSS file for styling
 
 const EventPlanner = () => {
@@ -7,6 +7,27 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+            <section className="description">
+                <p>
+                    Plan and organize your events effortlessly with Event Planner. From
+                    birthdays to corporate meetings, We&apos;ve got you covered.
+                </p>
+                <button className="get-started-button">Get Started</button>
+
+
+            </section>
+            <section className="events_categories">
+
+            </section>
+            <section className="features">
+
+            </section>
+            <section className="testimonials">
+
+            </section>
+            <section className="contact">
+
+            </section>
         </div>
     );
 };
